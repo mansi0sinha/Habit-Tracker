5fc89a7f984e60466817b7c961b8174cb1aa95f9
mansi Sinha <manshi0832@gmail.com>
2026-08-31 22:40:05 +0530

    Update auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,10 +1,15 @@
 import User from "../models/User.js";
 export const registerUser = async (req, res) => {
-    
+    try {
         const user = await User.create({
             email: req.body.email,
             password: req.body.password,
             timezone: req.body.timezone
         });
         res.send(user);
-    };
+    } catch (error) {
+        console.log(error);
+        res.send("Error occurred");
+    }
+
+};
