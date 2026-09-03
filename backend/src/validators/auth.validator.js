3ba17d6d05f775cbbf526dac3bfcfd45321cb198
mansi Sinha <manshi0832@gmail.com>
2026-09-03 12:45:22 +0530

    Update auth.validator.js

--- backend/src/validators/auth.validator.js
+++ backend/src/validators/auth.validator.js
@@ -2,6 +2,16 @@ import {z} from "zod";
 const registerSchema=z.object({
     email:z.email(),
     password:z.string().min(6),
-    timezone:z.string()
+    timezone:z.string().refine(value=>{
+        try{
+            Intl.DateTimeFormat(undefined,{
+                timeZone:value
+            });
+            return true;
+        }catch(error){
+            return false;
+        }
+        
+    })
 });
 export {registerSchema};
