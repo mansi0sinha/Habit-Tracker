75677718f3d76e0220a9368152210dae6c4aa788
mansi Sinha <manshi0832@gmail.com>
2026-09-05 12:29:51 +0530

    Authentication using tokens and middlewares

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,9 +1,23 @@
 import jwt from "jsonwebtoken";
-export const authMiddleware =(req,res,next)=>{
-const authHeader=req.headers.authorization;
-if(!authHeader){
-    return res.status(401).json({
-        message:"Authentication required"
-    });
-}
+export const authMiddleware = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).json({
+            message: "Authentication required"
+        });
+    }
+    try {
+        const decoded = jwt.verify(
+            authHeader.split(" ")[1],
+            process.env.JWT_SECRET
+        );
+         req.user=decoded.userId;
+         next();
+    } catch (error) {
+        return res.status(401).json({
+            message: "Authentication error"
+        });
+    }
+   
+
 }
--- backend/src/routes/test.routes.js
+++ backend/src/routes/test.routes.js
@@ -1,7 +1,13 @@
 import express from 'express';
-import {registerUser,loginUser} from "../controllers/auth.controller.js"
+import {registerUser,loginUser} from "../controllers/auth.controller.js";
+import {authMiddleware} from "../middleware/auth.middleware.js"
 const router = express.Router();
 //Routing
 router.post('/test-user',registerUser);
 router.post('/test-login',loginUser);
+router.get('/test-route',authMiddleware,(req,res)=>{
+    return res.status(200).json({
+        message:"You are authenticated"
+    });
+});
 export default router;
